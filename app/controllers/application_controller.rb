97ccd22531b81abc03b96a7e45fe1241b7d181c9
HankJiang <jianghan.ah@foxmail.com>
2021-05-23 20:08:18 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  before_filter :add_allow_credentials_headers
-
   before_action :authorized
   helper_method :current_user
   helper_method :logged_in?
@@ -16,12 +14,5 @@ class ApplicationController < ActionController::Base
 
   def authorized
     head :unauthorized unless logged_in?
-  end
-
-  private
-
-  def add_allow_credentials_headers
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'api.gsxxm.com'
+    origins 'localhost', 'http://api.gsxxm.com', 'http://gsxxm.com'
     resource '*',
              headers: :any,
              methods: [:get, :post, :patch, :put, :delete],
