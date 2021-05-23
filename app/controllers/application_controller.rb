eaee0abf1bd1efec687cb5cb57bd5bbc20f67dac
HankJiang <jianghan.ah@foxmail.com>
2021-05-23 19:48:53 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_filter :add_allow_credentials_headers
+
   before_action :authorized
   helper_method :current_user
   helper_method :logged_in?
@@ -14,5 +16,12 @@ class ApplicationController < ActionController::Base
 
   def authorized
     head :unauthorized unless logged_in?
+  end
+
+  private
+
+  def add_allow_credentials_headers
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
   end
 end
