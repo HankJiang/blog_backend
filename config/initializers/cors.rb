026a81d2b8d1774cb853502dc36d940b7d3a8865
HankJiang <jianghan.ah@foxmail.com>
2021-05-23 20:19:02 +0800

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost', 'http://api.gsxxm.com', 'http://gsxxm.com'
+    # origins 'localhost', 'http://api.gsxxm.com', 'http://gsxxm.com'
+    origins '*'
     resource '*',
              headers: :any,
-             methods: [:get, :post, :patch, :put, :delete],
-             credentials: true
+             methods: [:get, :post, :patch, :put, :delete]
+             # credentials: true
   end
 end
